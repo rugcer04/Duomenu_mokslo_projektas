c19b847e2a221eba2c914a429feb946ecf7a6974
Cristup <cristupkmz@gmail.com>
2026-03-30 20:22:45 +0300

    Enhance api_test.py to include datastore access and collection verification

--- code/python/EUMDAC/api_test.py
+++ code/python/EUMDAC/api_test.py
@@ -8,3 +8,15 @@ credentials = (CONSUMER_KEY, CONSUMER_SECRET)
 token = eumdac.AccessToken(credentials)
 
 print(f"This token '{token}' expires {token.expiration}")
+
+datastore = eumdac.DataStore(token)
+
+look = 'EO:EUM:DAT:METOP:GOMEL1'
+
+for collection in datastore.collections:
+	print(collection)
+
+collections = [col._id for col in datastore.collections]
+
+if look in collections:
+    print(f"Collection '{look}' is available")
